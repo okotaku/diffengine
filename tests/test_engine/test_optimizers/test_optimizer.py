e6742bce8e2f2697c9b00b93d577f2d6e56428a5
okotaku <to78314910@gmail.com>
2023-08-04 09:54:29 +0900

    add test optim

---
+++ tests/test_engine/test_optimizers/test_optimizer.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from diffengine.engine import TRANSFORMER_OPTIMIZERS
+
+
+class TestBuilder(TestCase):
+
+    def test_torch_optimizers(self):
+        torch_optimizers = [
+            'Adafactor',
+        ]
+        assert set(torch_optimizers).issubset(set(TRANSFORMER_OPTIMIZERS))
