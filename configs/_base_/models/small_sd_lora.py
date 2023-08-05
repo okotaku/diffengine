6374890159fa9a930ab633948ddaef96951d76c6
okotaku <to78314910@gmail.com>
2023-08-05 16:56:17 +0900

    support distill sd dreambooth

---
+++ configs/_base_/models/small_sd_lora.py
@@ -0,0 +1,4 @@
+model = dict(
+    type='StableDiffusion',
+    model='segmind/small-sd',
+    lora_config=dict(rank=8))
---
+++ configs/_base_/models/tiny_sd_lora.py
@@ -0,0 +1,4 @@
+model = dict(
+    type='StableDiffusion',
+    model='segmind/tiny-sd',
+    lora_config=dict(rank=8))
