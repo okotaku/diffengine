9f9b777f2129e3b5a66f1e00be143907a1a65ff3
okotaku <to78314910@gmail.com>
2023-08-31 20:57:09 +0900

    add project face expression

---
+++ projects/face_expression/_base_/face_expression_dataset.py
@@ -0,0 +1,33 @@
+train_pipeline = [
+    dict(type='torchvision/Resize', size=512, interpolation='bilinear'),
+    dict(type='RandomCrop', size=512),
+    dict(type='RandomHorizontalFlip', p=0.5),
+    dict(type='torchvision/ToTensor'),
+    dict(type='torchvision/Normalize', mean=[0.5], std=[0.5]),
+    dict(type='PackInputs'),
+]
+train_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    dataset=dict(
+        type='HFDataset',
+        dataset='data/ExpressionTraining',
+        pipeline=train_pipeline,
+        image_column='file_name'),
+    sampler=dict(type='DefaultSampler', shuffle=True),
+)
+
+val_dataloader = None
+val_evaluator = None
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
+
+custom_hooks = [
+    dict(
+        type='VisualizationHook',
+        prompt=[
+            '1girl, >_<, blue hair', '1girl, X X, blue hair',
+            '1girl, @_@, blue hair', '1girl, =_=, blue hair'
+        ]),
+    dict(type='LoRASaveHook')
+]
