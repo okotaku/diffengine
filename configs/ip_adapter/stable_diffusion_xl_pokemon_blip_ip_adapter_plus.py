91bc51043990f37e4c5173c2fa851bcfec3933d0
okotaku <to78314910@gmail.com>
2023-09-29 23:23:33 +0000

    support ip adapter plus

---
+++ configs/ip_adapter/stable_diffusion_xl_pokemon_blip_ip_adapter_plus.py
@@ -0,0 +1,10 @@
+_base_ = [
+    '../_base_/models/stable_diffusion_xl_ip_adapter_plus.py',
+    '../_base_/datasets/pokemon_blip_xl_ip_adapter.py',
+    '../_base_/schedules/stable_diffusion_xl_50e.py',
+    '../_base_/default_runtime.py'
+]
+
+train_dataloader = dict(batch_size=1)
+
+optim_wrapper_cfg = dict(accumulative_counts=4)  # update every four times
--- diffengine/models/editors/ip_adapter/ip_adapter_xl.py
+++ diffengine/models/editors/ip_adapter/ip_adapter_xl.py
@@ -11,6 +11,7 @@ from transformers import CLIPVisionModelWithProjection
 from diffengine.models.archs import set_unet_ip_adapter
 from diffengine.models.editors.ip_adapter.image_projection import \
     ImageProjModel
+from diffengine.models.editors.ip_adapter.resampler import Resampler
 from diffengine.models.editors.stable_diffusion_xl import StableDiffusionXL
 from diffengine.models.losses.snr_l2_loss import SNRL2Loss
 from diffengine.registry import MODELS
@@ -241,6 +242,156 @@ class IPAdapterXL(StableDiffusionXL):
                 1 - self.zeros_image_embeddings_prob
             ]), len(image_embeds)).to(image_embeds)
         image_embeds = image_embeds * mask
+
+        # todo: drop image
+        ip_tokens = self.image_projection(image_embeds)
+        prompt_embeds = torch.cat([prompt_embeds, ip_tokens], dim=1)
+
+        if self.scheduler.config.prediction_type == 'epsilon':
+            gt = noise
+        elif self.scheduler.config.prediction_type == 'v_prediction':
+            gt = self.scheduler.get_velocity(latents, noise, timesteps)
+        else:
+            raise ValueError('Unknown prediction type '
+                             f'{self.scheduler.config.prediction_type}')
+
+        model_pred = self.unet(
+            noisy_latents,
+            timesteps,
+            prompt_embeds,
+            added_cond_kwargs=unet_added_conditions).sample
+
+        loss_dict = dict()
+        # calculate loss in FP32
+        if isinstance(self.loss_module, SNRL2Loss):
+            loss = self.loss_module(
+                model_pred.float(),
+                gt.float(),
+                timesteps,
+                self.scheduler.alphas_cumprod,
+                weight=weight)
+        else:
+            loss = self.loss_module(
+                model_pred.float(), gt.float(), weight=weight)
+        loss_dict['loss'] = loss
+        return loss_dict
+
+
+@MODELS.register_module()
+class IPAdapterXLPlus(IPAdapterXL):
+
+    def prepare_model(self) -> None:
+        """Prepare model for training.
+
+        Disable gradient for some models.
+        """
+        self.image_encoder = CLIPVisionModelWithProjection.from_pretrained(
+            self.image_encoder_name, subfolder='image_encoder')
+        self.image_projection = Resampler(
+            embed_dims=self.image_encoder.config.hidden_size,
+            output_dims=self.unet.config.cross_attention_dim,
+            hidden_dims=1280,
+            depth=4,
+            head_dims=64,
+            num_heads=20,
+            num_queries=self.clip_extra_context_tokens,
+            ffn_ratio=4)
+        self.image_encoder.requires_grad_(False)
+        super(IPAdapterXL, self).prepare_model()
+
+    def _encode_image(self, image, num_images_per_prompt):
+        if not isinstance(image, torch.Tensor):
+            from transformers import CLIPImageProcessor
+            image_processor = CLIPImageProcessor.from_pretrained(
+                self.image_encoder_name, subfolder='image_processor')
+            image = image_processor(image, return_tensors='pt').pixel_values
+
+        image = image.to(device=self.device)
+        image_embeddings = self.image_encoder(
+            image, output_hidden_states=True).hidden_states[-2]
+        image_prompt_embeds = self.image_projection(image_embeddings)
+        uncond_image_embeddings = self.image_encoder(
+            torch.zeros_like(image),
+            output_hidden_states=True).hidden_states[-2]
+        uncond_image_prompt_embeds = self.image_projection(
+            uncond_image_embeddings)
+
+        # duplicate image embeddings for each generation per prompt, using mps
+        # friendly method
+        bs_embed, seq_len, _ = image_prompt_embeds.shape
+        image_prompt_embeds = image_prompt_embeds.repeat(
+            1, num_images_per_prompt, 1)
+        image_prompt_embeds = image_prompt_embeds.view(
+            bs_embed * num_images_per_prompt, seq_len, -1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.repeat(
+            1, num_images_per_prompt, 1)
+        uncond_image_prompt_embeds = uncond_image_prompt_embeds.view(
+            bs_embed * num_images_per_prompt, seq_len, -1)
+
+        return image_prompt_embeds, uncond_image_prompt_embeds
+
+    def forward(self,
+                inputs: torch.Tensor,
+                data_samples: Optional[list] = None,
+                mode: str = 'loss'):
+        assert mode == 'loss'
+        inputs['text_one'] = self.tokenizer_one(
+            inputs['text'],
+            max_length=self.tokenizer_one.model_max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt').input_ids.to(self.device)
+        inputs['text_two'] = self.tokenizer_two(
+            inputs['text'],
+            max_length=self.tokenizer_two.model_max_length,
+            padding='max_length',
+            truncation=True,
+            return_tensors='pt').input_ids.to(self.device)
+        num_batches = len(inputs['img'])
+        if 'result_class_image' in inputs:
+            # use prior_loss_weight
+            weight = torch.cat([
+                torch.ones((num_batches // 2, )),
+                torch.ones((num_batches // 2, )) * self.prior_loss_weight
+            ]).float().reshape(-1, 1, 1, 1)
+        else:
+            weight = None
+
+        latents = self.vae.encode(inputs['img']).latent_dist.sample()
+        latents = latents * self.vae.config.scaling_factor
+
+        noise = torch.randn_like(latents)
+
+        if self.enable_noise_offset:
+            noise = noise + self.noise_offset_weight * torch.randn(
+                latents.shape[0], latents.shape[1], 1, 1, device=noise.device)
+
+        timesteps = torch.randint(
+            0,
+            self.scheduler.num_train_timesteps, (num_batches, ),
+            device=self.device)
+        timesteps = timesteps.long()
+
+        noisy_latents = self.scheduler.add_noise(latents, noise, timesteps)
+
+        prompt_embeds, pooled_prompt_embeds = self.encode_prompt(
+            inputs['text_one'], inputs['text_two'])
+        unet_added_conditions = {
+            'time_ids': inputs['time_ids'],
+            'text_embeds': pooled_prompt_embeds
+        }
+
+        # random zeros image
+        clip_img = inputs['clip_img']
+        mask = torch.multinomial(
+            torch.Tensor([
+                self.zeros_image_embeddings_prob,
+                1 - self.zeros_image_embeddings_prob
+            ]), len(clip_img)).to(clip_img)
+        clip_img = clip_img * mask
+        # encode image
+        image_embeds = self.image_encoder(
+            clip_img, output_hidden_states=True).hidden_states[-2]
 
         # todo: drop image
         ip_tokens = self.image_projection(image_embeds)
