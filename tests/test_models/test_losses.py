80cc3db79b6400ccc70b5ab531662320f60658dc
okotaku <to78314910@gmail.com>
2023-10-11 10:27:26 +0000

    fix test

--- tests/test_models/test_losses.py
+++ tests/test_models/test_losses.py
@@ -22,7 +22,7 @@ def test_snr_l2_loss():
     weight = torch.Tensor([[1], [0.1]])
     timesteps = (torch.ones((pred.shape[0], )) + 10)
     scheduler = StableDiffusionPipeline.from_pretrained(
-        'runwayml/stable-diffusion-v1-5').scheduler
+        'diffusers/tiny-stable-diffusion-torch').scheduler
 
     loss = SNRL2Loss()
     assert torch.allclose(
