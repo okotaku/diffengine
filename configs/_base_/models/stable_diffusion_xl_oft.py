f4e1bc51be992f2d1ce0758857e556a28cec9192
okotaku <to78314910@gmail.com>
2023-11-27 02:18:09 +0000

    Support OFT

---
+++ configs/_base_/models/stable_diffusion_xl_oft.py
@@ -0,0 +1,9 @@
+model = dict(
+    type="StableDiffusionXL",
+    model="stabilityai/stable-diffusion-xl-base-1.0",
+    vae_model="madebyollin/sdxl-vae-fp16-fix",
+    unet_lora_config=dict(
+        type="OFT",
+        r=8,
+        alpha=1,
+        target_modules=["to_q", "to_v", "to_k", "to_out.0"]))
