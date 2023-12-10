655df21fa99e7a58106599aaf797b6c7fd4a7446
okotaku <to78314910@gmail.com>
2023-12-10 02:35:28 +0000

    Support ControlNetXS

--- diffengine/models/editors/stable_diffusion_xl_controlnet/__init__.py
+++ diffengine/models/editors/stable_diffusion_xl_controlnet/__init__.py
@@ -1,4 +1,6 @@
 from .sdxl_controlnet_data_preprocessor import SDXLControlNetDataPreprocessor
 from .stable_diffusion_xl_controlnet import StableDiffusionXLControlNet
+from .stable_diffusion_xl_controlnetxs import StableDiffusionXLControlNetXS
 
-__all__ = ["SDXLControlNetDataPreprocessor", "StableDiffusionXLControlNet"]
+__all__ = ["SDXLControlNetDataPreprocessor", "StableDiffusionXLControlNet",
+           "StableDiffusionXLControlNetXS"]
