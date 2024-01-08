a8c13410c333d86a1b1ab5617795eac6c376b144
okotaku <to78314910@gmail.com>
2024-01-08 02:11:24 +0000

    Fix test

--- tests/test_engine/test_hooks/test_ip_adapter_save_hook.py
+++ tests/test_engine/test_hooks/test_ip_adapter_save_hook.py
@@ -3,14 +3,13 @@ import os.path as osp
 import shutil
 from pathlib import Path
 
+from mmengine.config import Config
 from mmengine.model import BaseModel
 from mmengine.registry import MODELS
 from mmengine.testing import RunnerTestCase
 from torch import nn
 
 from diffengine.engine.hooks import IPAdapterSaveHook
-from diffengine.models.editors import IPAdapterXL, IPAdapterXLDataPreprocessor
-from diffengine.models.losses import L2Loss
 from diffengine.models.utils import TimeSteps, WhiteNoise
 
 
@@ -30,20 +29,12 @@ class TestIPAdapterSaveHook(RunnerTestCase):
 
     def setUp(self) -> None:
         MODELS.register_module(name="DummyWrapper", module=DummyWrapper)
-        MODELS.register_module(name="IPAdapterXL", module=IPAdapterXL)
-        MODELS.register_module(
-            name="IPAdapterXLDataPreprocessor",
-            module=IPAdapterXLDataPreprocessor)
-        MODELS.register_module(name="L2Loss", module=L2Loss)
         MODELS.register_module(name="WhiteNoise", module=WhiteNoise)
         MODELS.register_module(name="TimeSteps", module=TimeSteps)
         return super().setUp()
 
     def tearDown(self):
         MODELS.module_dict.pop("DummyWrapper")
-        MODELS.module_dict.pop("IPAdapterXL")
-        MODELS.module_dict.pop("IPAdapterXLDataPreprocessor")
-        MODELS.module_dict.pop("L2Loss")
         MODELS.module_dict.pop("WhiteNoise")
         MODELS.module_dict.pop("TimeSteps")
         return super().tearDown()
@@ -53,17 +44,10 @@ class TestIPAdapterSaveHook(RunnerTestCase):
 
     def test_before_save_checkpoint(self):
         cfg = copy.deepcopy(self.epoch_based_cfg)
-        cfg.model.type = "IPAdapterXL"
-        cfg.model.model = "hf-internal-testing/tiny-stable-diffusion-xl-pipe"
-        cfg.model.image_encoder = 'hf-internal-testing/unidiffuser-diffusers-test'  # noqa
-        cfg.model.image_encoder_sub_folder = "image_encoder"
+        cfg.model = Config.fromfile("tests/configs/ip_adapter.py").model
         runner = self.build_runner(cfg)
         checkpoint = dict(
-            state_dict=IPAdapterXL(
-                model="hf-internal-testing/tiny-stable-diffusion-xl-pipe",
-                image_encoder="hf-internal-testing/unidiffuser-diffusers-test",
-                image_encoder_sub_folder="image_encoder",
-            ).state_dict())
+            state_dict=MODELS.build(cfg.model).state_dict())
         hook = IPAdapterSaveHook()
         hook.before_save_checkpoint(runner, checkpoint)
 
